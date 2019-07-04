f6ea1867d3c60aa1d8ab598bbea16f3e935769eb
Simas <simonas.deimantavicius@mediapark.com>
2019-07-04 08:44:30 +0300

    setup basic workflow

---
+++ src/pages/Lesson1.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="lesson-1">
+    <h3>Lesson One</h3>
+    <svg width="600" height="600">
+      <rect x="300" y="100" fill="blue" width="100" height="200" />
+    </svg>
+  </section>
+</template>
+<script>
+export default {
+  name: 'LessonOne',
+};
+</script>
+<style>
+</style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>D3 Data Visualization Training</h1>
+</template>
+
+<script>
+export default {
+  name: 'PageHome',
+};
+</script>
+<style>
+</style>
